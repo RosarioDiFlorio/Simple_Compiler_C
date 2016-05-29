df657c3ba8df92af8b7e5c0d666554a7678e1850
Delkatis <v.venosi@gmail.com>
2016-05-29 23:18:17 +0200

    aggiornamenti parser

--- Lexer_cup_c/c_cup/test.c
+++ Lexer_cup_c/c_cup/test.c
@@ -1,3 +1,22 @@
-unsigned int a = 0;
-void ciao(int a, char b){}
-prova ciao;
+int mia = 4;
+mis = 3;
+unsigned int a = [mi *(5 + 3)];
+void ciao(int a, char b){
+    int a;
+    a = 10;
+    a + b;
+    
+}
+if(a == b){ a = a + 3;  }
+while(a >= 10){
+    a +=1;
+    
+}
+for( i = 0; i< 100; i  = i + 1){
+    
+    i += 1;
+}
+
+
+
+
